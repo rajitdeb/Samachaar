fe43250a1ff10496d0472d70040b444538723e93
Rajit Deb <rajitdeb456@gmail.com>
2023-11-22 17:20:43 +0530

    Added Thread-safety implementation code block for reference in FavouriteArticlesDatabase.kt

--- app/src/main/java/com/rajit/samachaar/data/local/database/FavouriteArticlesDatabase.kt
+++ app/src/main/java/com/rajit/samachaar/data/local/database/FavouriteArticlesDatabase.kt
@@ -18,4 +18,34 @@ abstract class FavouriteArticlesDatabase: RoomDatabase() {
 
     abstract fun favouriteArticleDao(): ArticlesDao
 
+    /**
+     * NOTE:
+     * If DI wasn't used in this project, then the Thread-safety and Singleton Instance creation
+     * would have been done in this way
+     */
+//    companion object {
+//
+//        /*
+//         * Volatile annotation is used to let all the threads know immediately about this instance
+//         * in a multi-threaded environment
+//         */
+//        @Volatile
+//        private var instance: MemeDatabase? = null
+//        private var LOCK = Any()
+//
+//        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+//            instance ?: createDatabase(context).also {
+//                instance = it
+//            }
+//        }
+//
+//        private fun createDatabase(context: Context) =
+//            Room.databaseBuilder(
+//                context.applicationContext,
+//                MemeDatabase::class.java,
+//                DATABASE_NAME
+//            ).build()
+//
+//    }
+
 }
